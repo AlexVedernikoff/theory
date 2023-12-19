e1e6750170dbd204f92cccd4596533564daaffca
AlexVedernikoff <templar_sc@mail.ru>
2023-12-19 03:15:59 +0300

    алгоритм дэйкстры

---
+++ src/Алгоритмы/Графы/Алгоритм Дэйкстры.tsx
@@ -0,0 +1,110 @@
+export const DijkstraReact = () => {
+  console.log("Алгоритм Дэйскстры");
+  // ******************************************************************
+  const nodesList = ["a", "b", "c", "d", "e", "i"] as const;
+  type TNodesList = (typeof nodesList)[number]; // "a" | "b" | "c" | "d" | "e" | "i"
+  type TNode = { [Node in TNodesList]?: number }; // например { a: 2, d: 3 }
+  type Tgraph = { [Node in TNodesList]: TNode };
+  // взвешенный граф (указаны расстояния между точками)
+  const graph: Tgraph = {
+    a: { b: 2, c: 1, i: 3 },
+    b: { a: 2, d: 3 },
+    c: { a: 1, d: 1 },
+    d: { b: 3, c: 3, e: 5 },
+    e: { d: 5, i: 2 },
+    i: { a: 3, e: 1 },
+  };
+
+  //   Создаём
+  // объект parents, который будет хранить историю переходов по графу,
+  // объект costs , который будет хранить стоимости переходов,
+  // массив queue — очередь обхода вершин.
+
+  // Простейший вариант функции. Находит наименьшую по стоимости цепочку
+  // от  start до end, не вычисляя само значение стоимости.
+  function dijkstra(graph: Tgraph, start: TNodesList, end: TNodesList) {
+    const distances: TNode = {}; // = costs (расстояние до точек / "стоимость" перехода между точками)
+    const visited = new Set<TNodesList>(); // Set(2) {'a', 'c'}  // = parents (сюда будем помещать посещённые вершины)
+    const path = {}; // = queue (история пути)
+
+    for (const key in graph) {
+      if (key !== start) {
+        distances[key] = Infinity;
+      } else {
+        distances[start] = 0;
+      }
+    }
+
+    console.table("distanses перед while = ");
+    console.table(distances);
+    // Заполняем объект distances.
+    // для "a" стоимость перехода = 0, т.к. это стартовая точка.
+    // для остальных вершин "b" ... "i" стоимость перехода = Infinity
+    // !visited.has(end) = пока мы не посетили последнюю вершину
+    let i = 0;
+    while (!visited.has(end)) {
+      console.log("\n\n****** Начинаем новый цикл while ****** ", i++);
+      let lowestDistance = Infinity;
+      let node: TNodesList | null = null;
+      let key: TNodesList;
+      console.log("lowestDistance = ", lowestDistance);
+      console.log("Теперь мы перебираем все not visited вершины в объекте  distances");
+
+      for (key in distances) {
+        if (lowestDistance > distances[key]! && !visited.has(key)) {
+          console.log("\nkey = ", key, ` distances[${key}] = ${distances[key]}`);
+          console.log(
+            `Так как lowestDistance=${lowestDistance} > distances[${key}]=${distances[key]}, 
+            то принимаем distances[${key}] в качестве нового значения lowestDistance`
+          );
+          lowestDistance = distances[key]!;
+          console.log(`И теперь значение lowestDistance = ${lowestDistance}`);
+          node = key;
+        }
+      }
+      console.log("Текущая вершина node = ", node);
+      const neighbours = graph[node!];
+      console.log(`neighbours cоседи вершины ${node}  = `, neighbours);
+
+      for (const key in neighbours) {
+        console.log(`Вычисляем новую дистанцию newDistance для ${key}`);
+        console.log(`distances[${node}] = `, distances[node!]);
+        console.log(`neighbours[${key}] = `, neighbours[key]);
+        const newDistance = distances[node!] + neighbours[key];
+        if (newDistance < distances[key]) {
+          console.log(
+            `Так как newDistance=${newDistance} < distances[${key}]=${distances[key]}, 
+            то назначем distances[${key}] новое значение newDistance`
+          );
+          distances[key] = newDistance;
+          console.log(`А в объект path по ключу ${key} записываем ${node}`);
+          path[key] = node;
+        }
+      }
+      node && visited.add(node);
+      console.log("visited = ", visited);
+      console.log("distances = ");
+      console.table(distances);
+      console.log("path = ");
+      console.table(path);
+    }
+
+    const shortPath: Array<TNodesList> = [];
+    let current = end;
+    while (current !== start) {
+      shortPath.unshift(current);
+      current = path[current];
+    }
+    shortPath.unshift(start);
+    console.log(`Количество операций = `, i);
+    return shortPath;
+  }
+
+  // *** Проверка ***
+  console.log(
+    "Самый короткий путь в графе между вершинами a и e = ",
+    dijkstra(graph, "a", "e")
+  );
+
+  // ******************************************************************
+};
