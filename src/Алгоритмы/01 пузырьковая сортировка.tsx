4b5ab2b547464a8ac812620ecbc7f06775bd69e9
AlexVedernikoff <templar_sc@mail.ru>
2023-12-23 03:26:17 +0300

    final пузырьковая сортировка

--- src/Алгоритмы/01 пузырьковая сортировка.tsx
+++ src/Алгоритмы/01 пузырьковая сортировка.tsx
@@ -1,29 +1,35 @@
 // *** Алгоритм пузырьковой сортировки ************************
 // Пузырьковая сортировка – один из самых неэффективных алгоритмов с количеством операций O(N²).
 // Смысл называния - большие числа, как большие пузырьки воздуха, всплывают наверх.
+// Является устойчиваой (стабильной) => не меняет относительный порядок элементов с одинаковыми значениями.
 
 import { createArray } from "../zПрочее/createArray";
 import { deepEqual } from "../Теория/deepEqual";
 
 export const bubbleSortReact = () => {
+  // **********************************************************
+  // *** самая простая реализация ***
+
   function bubbleSort(arr) {
+    function swap(i: number, j: number, arr: Array<any>) {
+      return ([arr[i], arr[j]] = [arr[j], arr[i]]);
+    }
     for (let j = arr.length - 1; j > 0; j--) {
       for (let i = 0; i < j; i++) {
+        // если текущий элемент массива больше следующего, то меняем их местами, иначе ничего не делаем.
         if (arr[i] > arr[i + 1]) {
-          let temp = arr[i];
-          arr[i] = arr[i + 1];
-          arr[i + 1] = temp;
+          swap(i, i + 1, arr);
         }
       }
     }
     return arr;
   }
-  //   ********************************************************
-
+  // *********************************************************************************
+  // *** Более продвинутый вариант. Если массив уже отсортирован, выходим из цикла ***
   function bubbleSortPro(array: Array<any>) {
     let sorted = false;
     let counter = 0;
-    //функция меняет местами два элемента в массиве
+    // функция swap() меняет местами два элемента в массиве
     function swap(i: number, j: number, arr: Array<any>) {
       return ([arr[i], arr[j]] = [arr[j], arr[i]]);
     }
@@ -42,20 +48,21 @@ export const bubbleSortReact = () => {
   }
 
   // *** Проверка ***
-  const list = createArray(10, 100);
+  const list = createArray(10, 10);
+
   console.log("Массив, который нужно отсортировать = ", list);
-  console.log("Результат сортировки = ", bubbleSort(list));
+  // console.log("Результат сортировки = ", bubbleSort([...list]));
   console.log(
     `Результат функции ${bubbleSort.name} совпадает с методом Array.sort = `,
     deepEqual(
-      bubbleSort(list),
+      bubbleSort([...list]),
       [...list].sort((a, b) => a - b)
     )
   );
   console.log(
     `Результат функции ${bubbleSortPro.name} совпадает с методом Array.sort = `,
     deepEqual(
-      bubbleSortPro(list),
+      bubbleSortPro([...list]),
       [...list].sort((a, b) => a - b)
     )
   );
@@ -67,3 +74,5 @@ export const bubbleSortReact = () => {
 // https://proglib.io/p/bubble-sort
 // 2-й способ, более изящный, с проверкой  sorted = true/false
 // https://stackoverflow.com/questions/37817334/javascript-bubble-sort
+// (3) таблица сложности разных алгоритмов сортировки
+// https://proglib.io/p/rasprostranennye-algoritmy-i-struktury-dannyh-v-javascript-osnovnye-ponyatiya-i-rabota-s-massivami-2021-10-06
